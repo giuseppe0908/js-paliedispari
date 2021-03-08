01c0fd5d959f8aeb9f7ebe53486bff5b0373bcfd
giuseppe <placidagiuseppe@gmail.com>
2021-03-08 19:10:36 +0100

    esercizio completato

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ function palindroma(nom){
   var nome_reverse = nom.split("").reverse().join("");
   for (var i = 0; i < nome.length; i++) {
      if (nome == nome_reverse) {
-       console.log("il nome è polindrome");
+       console.log(nome +" "+" è polindrome");
      }else {
-       console.log("il nome non è polindrome");
+       console.log(nome +" "+ " non è polindrome");
      }
   }
   return nome_reverse;
@@ -19,17 +19,26 @@ function palindroma(nom){
 var verdetto = prompt("inserisci pari o dispari");
 verdetto = verdetto.toLowerCase();
  if ((verdetto != "pari") && (verdetto != "dispari")) {
-    verdetto = prompt("Reiserisci ti prego pari o dispari");
+    verdetto = prompt("Ti prego Reiserisci  pari o dispari");
     verdetto = verdetto.toLowerCase();
   }
+  console.log(verdetto);
 //Utente deve scegliere un numero da 1 a 5
-var numero = parseInt(prompt("inserisci un numero da 1 a 5"));
-if (isNaN(numero) || numero > 5) {
-  numero = parseInt(prompt("reinserisci un numero da 1 a 5"));
+var flag = false;
+while (!flag) {
+  numero = parseInt(prompt("inserisci un numero da 1 a 5"));
+
+  if (isNaN(numero) || numero > 5 || numero <= 0) {
+    numero = parseInt(prompt("reinserisci un numero da 1 a 5"));
+  }else {
+    flag = true;
+  }
 }
+console.log("Il numero che hai scelto è:"+" "+ numero);
+
 var somma = 0;
 //generiamo un numero random attraverso la funzione random per il computer
-random(1,6);
+random(1,5);
 function random(min , max){
   if (isNaN(min) && (max)) {
     console.log("Non sono numeri");
@@ -38,18 +47,20 @@ function random(min , max){
     return numero_random;
   }
 }
-console.log(numero_random);
-somma = numero + numero_random;
-console.log(somma);
+console.log("Il numero random del computer è:" +" "+ numero_random);
+// somma = numero + numero_random;
+// console.log(somma);
 //Creiamo una funzione che verifica se la somma dei due numeri è pari o dispari
-paliedispari(somma);
+paliedispari(numero, numero_random);
 
-function paliedispari(calcolo){
-  if (!(calcolo%2) && (verdetto == "pari")) {
+function paliedispari(n1, n2){
+  somma = n1 + n2;
+  console.log("La somma è:"+" "+somma);
+  if (!(somma%2) && (verdetto == "pari")) {
        console.log("il numero è pari hai vinto");
-  }else if(!(calcolo%2) && (verdetto == "dispari")) {
+  }else if(!(somma%2) && (verdetto == "dispari")) {
       console.log("il numero è pari tu hai scelto dispari hai perso");
-  }else if ((calcolo%2) && (verdetto == "dispari")) {
+  }else if ((somma%2) && (verdetto == "dispari")) {
        console.log("il numero è dispari hai vinto");
   }else {
       console.log("il numero è dispari hai perso");
